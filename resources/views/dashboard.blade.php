ea6a844374b5435a7444eca8efe20dc94f49cbed
alifadhul68 <202003689@student.polytechnic.bh>
2023-11-23 16:47:43 +0300

    make trial end text dynamic

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
     <div class="container mt-5">
         Hello, {{ auth()->user()->name }}
         @if(Auth::check() && auth()->user()->user_type == 'employer')
-            <p>Your subscription ends on: {{ auth()->user()->user_trial }}</p>
+            <p>Your subscription {{now()->format('Y-m-d') > auth()->user()->user_trial ? 'ended': 'ends'}} on: {{ auth()->user()->user_trial }}</p>
         @endif
         <div class="row justify-content-center">
 
